cf94afc70205db0554a418c580bc4642304a02cc
sonusharma91824 <sonusharma91824@gmail.com>
2022-09-12 18:55:39 +0530

    hello

---
+++ testclass/nonstatickeyword/Method2.java
@@ -0,0 +1,19 @@
+package testclass.nonstatickeyword;
+
+public class Method2 {
+	public void sonu(String name) 
+	{
+		System.out.println("Name of the student is " +name);
+	}
+	
+	public void mayank(int rollno)
+	{
+		System.out.println("Rollno of the student is " +rollno);
+	}
+	
+	public void khushi(int age)
+	{
+		System.out.println("Age of the student is " +age);
+	}
+
+}
