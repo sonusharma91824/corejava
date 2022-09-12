a3dd7d39a576726cc3a5778182b20e3cafbbd5f7
sonusharma91824 <sonusharma91824@gmail.com>
2022-09-12 18:47:36 +0530

    array

---
+++ com/Arrrrr.java
@@ -0,0 +1,13 @@
+package array.in.com;
+
+import java.util.Arrays;
+
+public class Arrrrr {
+public static void main(String[] args) {
+
+	int c2[] = {4,8,3,2,5,9};
+	Arrays.sort(c2);
+	System.out.println(c2[c2.length-1]);
+
+}
+}
